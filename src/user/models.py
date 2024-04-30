6d7128d9be6e5e24b378967eec1031e1b04d7bfd
Your Name <your.email@example.com>
2024-04-30 20:37:06 +0300

    fix user note relationship

--- src/user/models.py
+++ src/user/models.py
@@ -1,16 +1,21 @@
 """
 User models
 """
-
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from src.core import Base
 
+if TYPE_CHECKING:
+    from ..note.models import Note
 
 class User(Base):
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
+
+    notes: Mapped[list["Note"]] = relationship(back_populates="user")
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}(email={self.email})"
