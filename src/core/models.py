ec37365ba0b93a4defad8d8ab4caa5fa16e253f7
Your Name <your.email@example.com>
2024-04-30 18:05:43 +0300

    fix base model

--- src/core/models.py
+++ src/core/models.py
@@ -10,9 +10,8 @@ from sqlalchemy.orm import mapped_column
 
 class Base(DeclarativeBase):
     __abstract__ = True
-
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
