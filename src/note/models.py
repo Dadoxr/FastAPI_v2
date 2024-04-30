dc65b86060395e05b81b80050dd54b0ddcae73ef
Your Name <your.email@example.com>
2024-04-30 18:08:28 +0300

    create note model

---
+++ src/note/models.py
@@ -0,0 +1,16 @@
+"""
+Note models
+"""
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from ..core.models import Base
+from ..user.mixins import UserRelationMixin
+
+class Note(UserRelationMixin, Base):
+    _user_back_populates = "notes"
+
+    title: Mapped[str] = mapped_column(String(50))
+    body: Mapped[str]
