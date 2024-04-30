8316f0e3de89e0fa83dcb6a8122842b33994dd4f
Your Name <your.email@example.com>
2024-04-30 20:45:20 +0300

    setup rest (user, note)

--- src/note/REST/views.py
+++ src/note/REST/views.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 
 from ..schemas import NoteOutput
-
+from ..services import NoteService
 
 note_rest_router = APIRouter(prefix="/notes")
 
-@note_rest_router.get("/get_all")
+
+@note_rest_router.get("/all")
 async def get_all() -> list[NoteOutput | None]:
-    return [NoteOutput(title="REST", body=" не настроен")]
+    return await NoteService.get_all()
+
+
+@note_rest_router.get("/{note_id}")
+async def get_by_id(note_id: int) -> NoteOutput | None:
+    return await NoteService.get_by_id(note_id=note_id)
--- src/user/REST/views.py
+++ src/user/REST/views.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter
 
-from ..schemas import UserOutput
+from ..models import User
+from ..services import UserService
+from ..schemas import UserOutput, UserBaseInput
 
 user_rest_router = APIRouter(prefix="/users")
 
 
-@user_rest_router.get("/get_all")
+@user_rest_router.get("/all")
 async def get_all() -> list[UserOutput | None]:
-    return [UserOutput(email="REST не настроен")]
+    return await UserService.get_all()
+
+
+@user_rest_router.get("/{user_id}")
+async def get_by_id(user_id: int) -> UserOutput | None:
+    return await UserService.get_by_id(user_id=user_id)
+
+
+@user_rest_router.get("/{email}")
+async def get_by_email(user: UserBaseInput) -> UserOutput | None:
+    return await UserService.get_by_email(email=user.email)
