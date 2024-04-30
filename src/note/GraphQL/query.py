1c7840dc4c147d607041465f7cc5b71e0a4dd772
Your Name <your.email@example.com>
2024-04-30 20:45:34 +0300

    setup graphql (user, note)

--- src/note/GraphQL/query.py
+++ src/note/GraphQL/query.py
@@ -1,10 +1,17 @@
 import strawberry
 
 from ..schemas import NoteOutput
+from ..services import NoteService
+
 
 @strawberry.type
 class Query:
-    
+
     @strawberry.field
     async def get_all_notes() -> list[NoteOutput | None]:
-        return [NoteOutput(title="GraphQL", body=" не настроен")]
+        return await NoteService.get_all()
+
+    @strawberry.field
+    async def get_note_by_id(note_id: int) -> NoteOutput | None:
+        return await NoteService.get_by_id(note_id=note_id)
+
--- src/user/GraphQL/query.py
+++ src/user/GraphQL/query.py
@@ -1,11 +1,33 @@
 import strawberry
 
-from ..schemas import UserOutput
-
+from ...core import factory
+from ..dals import UserDAL
+from ..schemas import UserBaseInput, UserOutput
+from ..models import User
 
 @strawberry.type
 class Query:
 
     @strawberry.field
     async def get_all_users() -> list[UserOutput | None]:
-        return [UserOutput(email="GraphQL не настроен")]
+        async with factory.get_session() as session:
+            users: list[User | None] = await UserDAL.get_all(session=session)
+        return [UserOutput(email=user.email) for user in users]
+
+    @strawberry.field
+    async def get_user_by_id(user_id: int) -> UserOutput | None:
+        async with factory.get_session() as session:
+            user: User | None = await UserDAL.get_by_id(
+                session=session, user_id=user_id
+            )
+        if user:
+            return UserOutput(email=user.email)
+
+    @strawberry.field
+    async def get_user_by_email(user: UserBaseInput) -> UserOutput | None:
+        async with factory.get_session() as session:
+            user: User | None = await UserDAL.get_by_email(
+                session=session, email=user.email
+            )
+        if user:
+            return UserOutput(email=user.email)
